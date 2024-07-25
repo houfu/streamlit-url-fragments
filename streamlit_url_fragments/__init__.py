95be4438fadc480f34933334e180e09373a64b6e
houfu <houfu@users.noreply.github.com>
2024-07-25 23:22:13 +0800

    Bug: NoneType detection did not work

--- streamlit_url_fragments/__init__.py
+++ streamlit_url_fragments/__init__.py
@@ -19,7 +19,7 @@ def split_string_to_dict(input_string):
 
 def get_fragments() -> Union[str, dict, None]:
     component_value = _component_func()
-    if not None:
+    if not component_value:
         import re
         pattern = r'^#[^=&]+=[^&=]+(?:&[^=&]+=[^&=]+)*$'
         if re.match(pattern, component_value):
